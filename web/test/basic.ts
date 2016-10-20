6ff1f4e368a3a864db56bc418eabacc6289b3a83
纳兰天忆 <103520146@qq.com>
2016-10-20 16:40:02 +0800

    ts learning

---
+++ web/test/basic.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by nalantianyi on 2016/10/20.
+ */
+//let foo:number = 42;
+//foo = "42";
+let foo:any;
+foo = {};
+foo = 'bar';
+foo += 42;
+console.log(foo);
---
+++ web/test/enum.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by nalantianyi on 2016/10/20.
+ */
+enum STATES{
+    CONNENTING,
+    CONNECTED,
+    DISCONNECTING,
+    WAITING,
+    DISCONNECTED
+}
+;
