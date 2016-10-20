2e1bc3a95ec92c0bd728e587f65ac43ca46b4562
纳兰天忆 <103520146@qq.com>
2016-10-20 19:38:59 +0800

    ts learning

---
+++ web/test/function.js
@@ -0,0 +1,4 @@
+/**
+ * Created by nalantianyi on 2016/10/20.
+ */
+var isPrime = function (n) { };
---
+++ web/test/function.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by nalantianyi on 2016/10/20.
+ */
+// function isPrime(n:number):boolean {
+//
+// }
