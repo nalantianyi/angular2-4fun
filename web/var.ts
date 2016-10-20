dfec2b2aa1a6bdc574b644e8d9fe33a8ce08412a
纳兰天忆 <103520146@qq.com>
2016-10-20 15:43:35 +0800

    ts learning

---
+++ web/var.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by nalantianyi on 2016/10/20.
+ */
+var fns = [];
+for (var i = 0; i < 5; i++) {
+    fns.push(function () {
+        console.log(i);
+    });
+}
+fns.forEach(fn=>fn());
